1825bb21846f016beae7205ec42f97b4b3b11c6b
aqilakhalisa <79243614+aqilakhalisa@users.noreply.github.com>
2021-02-23 10:14:06 +0800

    Tes code

---
+++ Gas_in_github.js
@@ -0,0 +1,12 @@
+prosesPesan(update){
+  if(c = /(edited_)*(message|channel_post)/g.exec(Object.keys(update).toString())){
+    var msg = update[c[0]];
+    if(msg.text){
+      var text = msg.text;
+      if(/^\/start/g.exec(text)){
+        return tg.sendMsg(msg, 'hai bot ini di buat pake gas dengan script Dari github');
+      }
+    }
+    return tg.sendMsg(msg, 'Hai!');
+  }
+}
