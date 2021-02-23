c61b2f0d2978353ac9bb7d9f3904a97939d3578b
aqilakhalisa <79243614+aqilakhalisa@users.noreply.github.com>
2021-02-23 10:58:24 +0800

    Update Gas_in_github.js

--- Gas_in_github.js
+++ Gas_in_github.js
@@ -1,12 +1,12 @@
-prosesPesan(update){
+function prosesPesan(update){
   if(c = /(edited_)*(message|channel_post)/g.exec(Object.keys(update).toString())){
     var msg = update[c[0]];
     if(msg.text){
       var text = msg.text;
       if(/^\/start/g.exec(text)){
-        tg.sendMsg(msg, 'hai bot ini di buat pake gas dengan script Dari github');
+        return tg.sendMsg(msg, 'hai bot ini di buat pake gas dengan script Dari github');
       }
     }
-    tg.sendMsg(msg, 'Hai!');
+    return tg.sendMsg(msg, 'Hai!');
   }
 }
