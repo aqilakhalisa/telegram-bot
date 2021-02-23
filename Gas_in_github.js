4cec426692a3af3f285d3ca8d9426e6db5847b5f
aqilakhalisa <79243614+aqilakhalisa@users.noreply.github.com>
2021-02-23 10:43:12 +0800

    Hadeh

--- Gas_in_github.js
+++ Gas_in_github.js
@@ -4,9 +4,9 @@
     if(msg.text){
       var text = msg.text;
       if(/^\/start/g.exec(text)){
-        return tg.sendMsg(msg, 'hai bot ini di buat pake gas dengan script Dari github');
+        tg.sendMsg(msg, 'hai bot ini di buat pake gas dengan script Dari github');
       }
     }
-    return tg.sendMsg(msg, 'Hai!');
+    tg.sendMsg(msg, 'Hai!');
   }
 //}
